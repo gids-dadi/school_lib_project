46921fbbc81cdfc030ab6f7b67de38eb0ed562e3
gids-dadi <gideons564@gmail.com>
2022-08-31 23:28:11 +0100

    Correct the linter errors

--- person_class.rb
+++ person_class.rb
@@ -1,38 +1,27 @@
 require_relative './student_class'
 require_relative './teacher_class'
 
-class Person 
-  def initialize(age, parent_permission  = true, name = "Unknown"  )
-@id = Random.rand(1..1000)
-@name = name
-@age = age
-@parent_permission = parent_permission 
+class Person
+  def initialize(age, parent_permission = true, name = 'Unknown')
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
 
-    def id
-    @id
+  attr_reader :id, :name, :age
+
+  def name(value)
+    @name = value
   end
 
-    def name
-    @name
+  def age(value)
+    @age = value
   end
 
-    def age
-    @age
-end
-
-def name(value)
-  @name = value
-end
-
-def age(value)
-  @age = value
-end
-
-
-def is_of_age?
-  @age >= 18
-end
+  def is_of_age?
+    @age >= 18
+  end
 
   def can_use_services?
     @parent_permission == true || is_of_age?
